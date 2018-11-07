1d0ca24d5b497b7de64e16218f8b6ab2bdc6ca4f
running-bugs <36567382+running-bugs@users.noreply.github.com>
2018-11-07 17:40:14 +0800

    Update use.js

--- util/use.js
+++ util/use.js
@@ -1,30 +1,64 @@
-<!-- 定义一个类 方便操作节点元素 -->
+<!-- 定义一个类 -->
 var Doc_=function(){
-  /**
-   * ele属性 用来存储元素 -->
-   */
-  this.ele=document;
+	/**
+	 * ele属性 用来存储元素 -->
+	 */
+	this.ele=document;
+	
+	/**
+	 * 获取id元素
+	 */
+	this.get=function(tb){
+		return this.gets("#"+tb)[0];
+	}
+	
+	/**
+	 * 获取Doc_类下的子节点 并返回一个Doc_对象数组
+	 * Doc_默认元素是document-->
+	 */
+	this.gets=function(tb){
+		var eles;
+		
+		if(tb.indexOf(".")==0){
+			eles=this.ele.getElementsByClassName(tb.substr(1));
+		}else if(tb.indexOf("#")==0){
+			eles=this.ele.getElementById(tb.substr(1));
+		}else if(tb.indexOf("]")==0){
+			eles=this.ele.getElementsByName(tb.substr(1));
+		}else{
+			eles=this.ele.getElementsByTagName(tb);
+		}
 
-  /**
-   * 获取元素子节点 返回一个Doc_对象 通过
-   * Doc_默认元素是document-->
-   */
-  this.get=function(tb){
-    if(tb==null){
-      return this;
-    }
-    if(tb.indexOf(".")==0){
-      this.ele=this.ele.getElementsByClassName(tb.substr(1));
-    }else if(tb.indexOf("#")==0){
-      this.ele=this.ele.getElementById(tb.substr(1));
-    }else if(tb.indexOf("]")==0){
-      this.ele=this.ele.getElementsByName(tb.substr(1));
-    }else{
-      this.ele=this.ele.getElementsByTagName(tb);
-    }
-    return this;
-  };
+
+		var docs=new Array();
+		/**
+		 * 如果结果为null 则返回自己
+		 */
+		if(eles==null||eles.length==0){
+			return docs[0]=this;
+		}
+		if(eles.length==null){
+			docs[0]=new Doc_();
+			docs[0].ele=eles;
+			return docs;
+		}
+		
+		for(var i=0;i<eles.length;i++){
+			docs[i]=new Doc_();
+			docs[i].ele=eles[i];
+		}
+		
+		return docs;
+	};
+	
+	
+	/**
+	 * 创建元素
+	 */
+	var create = function(){
+		var new_=
+	}
 }
 
-/** 实例化Doc_类 */
+/** 实例化Doc_()类 **/
 var doc=new Doc_();
